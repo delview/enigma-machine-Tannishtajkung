664cd579c525bcbc68d3ed0bfe00fa28667adb8f
Tannishtajkung <tannishtas619@deltalearns.ca>
2025-03-05 13:30:08 -0800

    Added psueducode
    Added the major and basic pseudocode along with choosing the Atbash Cipher technique

---
+++ enigma_machine.py
@@ -0,0 +1,24 @@
+
+"""
+A program that will encyrpt or decrypt a message using the Atbash Cipher technique
+"""
+# Cipher technique used
+
+
+
+# Greet the user and explain them what this program does
+
+
+# Ask the user whether they want to decrypt or encrypt a message
+
+
+# Create two functions that will encrypt and decrypt messages 
+
+
+# Present the final encrypted or decrypted message to user
+
+
+# Ask if they want to do another encryption or decryption
+
+
+# Goodbye
