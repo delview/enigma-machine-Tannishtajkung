de4ee5727cdb8509c85e76606e8501dcdaae2421
Tannishtajkung <tannishtas619@deltalearns.ca>
2025-05-20 03:55:45 +0000

    Added all the letters in a dictionary

--- enigma_machine.py
+++ enigma_machine.py
@@ -2,7 +2,35 @@
 """
 A program that will encyrpt or decrypt a message using the Atbash Cipher technique
 """
-
+# Atbash dictionary from A to Z
+atbash_dict = {
+    'A': 'Z',
+    'B': 'Y',
+    'C': 'X',
+    'D': 'W',
+    'E': 'V',
+    'F': 'U',
+    'G': 'T',
+    'H': 'S',
+    'I': 'R',
+    'J': 'Q',
+    'K': 'P',
+    'L': 'O',
+    'M': 'N',
+    'N': 'M',
+    'O': 'L',
+    'P': 'K',
+    'Q': 'J',
+    'R': 'I',
+    'S': 'H',
+    'T': 'G',
+    'U': 'F',
+    'V': 'E',
+    'W': 'D',
+    'X': 'C',
+    'Y': 'B',
+    'Z': 'A'
+}
 # Cipher technique used
 def atbash_encrypt(): # Function that encrypts the message
     message_input = input("Enter a message that you want to encyrpt: ").strip().upper()
